32531e5a34921e01206f7d08f1c7475fcd17ef0b
M20 <m20@M20s-MacBook-Pro.local>
2022-02-25 22:48:58 +0200

    First lesson

---
+++ src/com/company/Main.java
@@ -0,0 +1,55 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    // write your code here
+
+        // Java is object-oriented programming language  that allows us to model
+        // variables and data types
+        // methods
+        // classes
+        // loops
+        // conditionals
+
+        // Variables and DataTypes
+        // Primitive Values
+        // byte
+        // short
+        // char
+        // int (Integer) for natural numbers 1, 2, 3...9
+        // long
+        // float for numbers with decimals max of 7 decimal places
+        // double for numbers with decimals, max of 15 decimal places
+
+        //Complex Objects
+        // Strings
+
+        // Variable: Think of a variable like an empty box, where you can put in only one type of items
+        // A variable declaration is simply giving your variable a name and a data type.
+        // A variable initialization is the point where you finally give that variable a value to start with.
+
+        int number; // variable declaration
+        number = 4; // variable initialization
+        int value = 6; // variable declaration and variable initialization in one line
+
+        // char charValue = '9';
+        // String text = "I begin my lesson";
+        //ASCII table for character values
+
+        // long digits = 5L;
+
+        // float decimalFloat = 3.683f;
+        // double decimalDouble = 5.23345677;
+
+        // Arithmetic Operators includes:
+        // + Addition
+        // - Substract
+        // / Division
+        // * multiply
+        // % Molulus (it divides the values and returns the remainder) e.g 2%2 ->0
+
+        System.out.println("Hello world");
+        System.out.println("Addition of number and value is: " + (number + value));
+    }
+}
